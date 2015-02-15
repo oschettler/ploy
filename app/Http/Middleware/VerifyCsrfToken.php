01218ebb9f7172a33e0110b329a4a963de0b30e3
Olav Schettler <olav.schettler@cocomore.com>
2015-02-15 17:13:47 +0100

    Prevent CSRF checking for API

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -5,6 +5,12 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier {
 
+    /*
+     * Array of Routes to skip CSRF check
+     * @see http://www.laravel-tricks.com/tricks/disable-csrf-on-specific-routes
+     */
+    private $openRoutes = ['/'];
+
 	/**
 	 * Handle an incoming request.
 	 *
@@ -14,6 +20,12 @@ class VerifyCsrfToken extends BaseVerifier {
 	 */
 	public function handle($request, Closure $next)
 	{
+        foreach($this->openRoutes as $route) {
+
+            if ($request->is($route)) {
+                return $next($request);
+            }
+        }
 		return parent::handle($request, $next);
 	}
 
