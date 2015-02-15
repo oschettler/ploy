539892fecb44e5ba4317074ea7fb17d31402d895
Olav Schettler <olav.schettler@cocomore.com>
2015-02-15 16:50:41 +0100

    Formatierung

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,74 +9,76 @@ class WelcomeController extends Controller
 
   const STASH_REPO_URL = 'https://{stash_user}:{stash_password}@jira.chefkoch.de/stash/rest/api/1.0/projects/{project_key}/repos/{repo_slug}';
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('guest');
-	}
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
 
-	/**
-	 * Show the application welcome screen to the user.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		return view('welcome');
-	}
-	
-	private function decodeInfo($info)
-	{	
-  	$repository = $info->repository;
-  	$project = $repository->project;
-  	$owner = $project->owner;
+    /**
+     * Show the application welcome screen to the user.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
 
-		$branch_name = join('/',
-			array_slice(
-				explode('/', $info->refChanges[0]->refId),
-				2
-			)
-		);
-		
-		$repo_url = strtr(self::STASH_REPO_URL, [
-	        '{stash_user}' => getenv('STASH_USER'),
-	        '{stash_password}' => getenv('STASH_PASSWORD'),
-	        '{project_key}' => $project->key,
-	        '{repo_slug}' => $repository->slug,
-		]);
-		$repo_info = json_decode(file_get_contents($repo_url));
-		
-		$ssh_clone_url = null;
-		foreach ($repo_info->links->clone as $url) {
-	        if ($url->name == 'ssh') {
-	            $ssh_clone_url = $url->href;
-	        }
-		}
+    private function decodeInfo($info)
+    {
+        $repository = $info->repository;
+        $project = $repository->project;
+        $owner = $project->owner;
 
-		return (object)[
-			'ssh_clone_url' => $ssh_clone_url,
-			'name' => $repository->name,
-			'owner_name' => $owner->displayName,
-			'owner_email' => $owner->emailAddress,
-			'branch_name' => $branch_name,
-		];
-	}
+        $branch_name = join('/',
+            array_slice(
+                explode('/', $info->refChanges[0]->refId),
+                2
+            )
+        );
 
-	public function webhook(Request $request)
-	{
-		$info = $this->decodeInfo(json_decode($request->getContent()));
-		$update = Update::createFromInfo($info);
-		return 'OK';
-	}
+        $repo_url = strtr(self::STASH_REPO_URL, [
+            '{stash_user}' => getenv('STASH_USER'),
+            '{stash_password}' => getenv('STASH_PASSWORD'),
+            '{project_key}' => $project->key,
+            '{repo_slug}' => $repository->slug,
+        ]);
+        $repo_info = json_decode(file_get_contents($repo_url));
 
-	public function t()
-	{
-	    $info = $this->decodeInfo(json_decode(file_get_contents('/tmp/branches.log')));
-		Update::createFromInfo($info);
+        $ssh_clone_url = null;
+        foreach ($repo_info->links->clone as $url) {
+            if ($url->name == 'ssh') {
+                $ssh_clone_url = $url->href;
+            }
+        }
 
-	}
+        return (object)[
+            'ssh_clone_url' => $ssh_clone_url,
+            'name' => $repository->name,
+            'owner_name' => $owner->displayName,
+            'owner_email' => $owner->emailAddress,
+            'branch_name' => $branch_name,
+        ];
+    }
+
+    public function webhook(Request $request)
+    {
+        error_log(strftime("%Y-%m-%d %H:%M:%S Webhook start\n"), 3, '/tmp/branches-log.log');
+        $info = $this->decodeInfo(json_decode($request->getContent()));
+        $update = Update::createFromInfo($info);
+        error_log(strftime("%Y-%m-%d %H:%M:%S Webhook finished\n"), 3, '/tmp/branches-log.log');
+        return 'OK';
+    }
+
+    public function t()
+    {
+        $info = $this->decodeInfo(json_decode(file_get_contents('/tmp/branches.log')));
+        Update::createFromInfo($info);
+
+    }
 }
