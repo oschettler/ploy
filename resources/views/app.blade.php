3226fb9f5ee0d6938cefb68fffa3853369e199a3
Olav Schettler <olav.schettler@pixelhouse.de>
2015-03-16 09:40:28 +0100

    Branches -> Ploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Branches</title>
+	<title>Ploy</title>
 
 	<link href="/css/app.css" rel="stylesheet">
 
@@ -28,7 +28,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Branches</a>
+				<a class="navbar-brand" href="#"><img src="/img/ck-ploy.png"></a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
